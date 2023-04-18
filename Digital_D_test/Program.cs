c05325ed22bd96a03b2df64e21671072a7cc4325
Олег Пересадин <operesadin66@outlook.com>
2023-04-18 23:47:43 +0300

    Добавьте файлы проекта.

---
+++ Digital_D_test/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace DD
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string inputTxt = @"C:\Users\Anast\OneDrive\Рабочий стол\Digital_D_test\tolstoj_lew_nikolaewich-text_0040.fb2";
+            string outputTxt = @"C:\Users\Anast\OneDrive\Рабочий стол\Digital_D_test\output2.txt";
+
+
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            //Regex regex = new Regex(@"\b[a-zа-яё'-]+\b", RegexOptions.IgnoreCase); //"output1.txt"
+            Regex regex = new Regex(@"\b[а-яё]+(-[а-яё]+)*\b", RegexOptions.IgnoreCase); //"output2.txt"
+
+            using (StreamReader sr = new StreamReader(inputTxt))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    foreach (Match match in regex.Matches(line))
+                    {
+                        string word = match.Value.ToLower();
+
+                        if (words.ContainsKey(word))
+                            words[word]++;
+                        else
+                            words[word] = 1;
+                    }
+                }
+            }
+
+
+
+            using (StreamWriter sw = new StreamWriter(outputTxt))
+            {
+                sw.WriteLine($"Всего уникальных слов: {words.Count}\n");
+
+                foreach (var item in words.OrderByDescending(k => k.Value))
+                {
+                    sw.WriteLine($"{item.Key}\t{item.Value}");
+                }
+            }
+        }
+
+    }
+
+}
